d314a03802031cc486b3b1f3e824c01e6c7e0219
Thalles Rafael <thallesrafaell@outlook.com>
2024-12-28 18:45:43 -0300

    feat: Adciona Sidebar desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
   return (
     <>
       <h1 className="text-2xl">DashBoard</h1>
-      <Button>Clique aqui</Button>
     </>
   );
 }
--- src/components/ui/sidebar/index.tsx
+++ src/components/ui/sidebar/index.tsx
@@ -3,16 +3,113 @@ import { Button } from "../button";
 import { Sheet, SheetTrigger, SheetContent } from "../sheet";
 import {
   Home,
+  LogOutIcon,
   Package,
   PanelBottom,
   Settings2,
   ShoppingBag,
   Users,
 } from "lucide-react";
+import {
+  Tooltip,
+  TooltipProvider,
+  TooltipContent,
+  TooltipTrigger,
+} from "../tooltip";
 
 export function SideBar() {
   return (
     <div className="flex w-full flex-col bg-muted/40">
+      <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 border-r bg-background sm:flex">
+        <nav className="flex flex-col items-center gap-4 px-2 py-5">
+          <TooltipProvider>
+            <Link
+              href="#"
+              className="flex h-9 w-9 items-center  shrink-0 justify-center bg-primary text-primary-foreground rounded-full"
+            >
+              <Package className="h-4 w-4" />
+              <span className="sr-only">Dashboard Avatar</span>
+            </Link>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 items-center shrink-0 justify-center  rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                >
+                  <Home className="h-4 w-4" />
+                  <span className="sr-only">Inicio</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">Inicio</TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 items-center shrink-0 justify-center  rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                >
+                  <ShoppingBag className="h-4 w-4" />
+                  <span className="sr-only">Pedidos</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">Pedidos</TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 items-center shrink-0 justify-center  rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                >
+                  <Package className="h-4 w-4" />
+                  <span className="sr-only">Produtos</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">Produtos</TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 items-center shrink-0 justify-center  rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                >
+                  <Users className="h-4 w-4" />
+                  <span className="sr-only">Clientes</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">Clientes</TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 items-center shrink-0 justify-center  rounded-lg text-muted-foreground transition-colors hover:text-foreground"
+                >
+                  <Settings2 className="h-4 w-4" />
+                  <span className="sr-only">Configuracoes</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">Configuracoes</TooltipContent>
+            </Tooltip>
+
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 items-center shrink-0 justify-center  rounded-lg text-muted-foreground transition-colors hover:text-foreground mt-auto"
+                >
+                  <LogOutIcon className="h-4 w-4 text-red-500" />
+                  <span className="sr-only">Sair</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">Sair</TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </nav>
+      </aside>
       <div className="sm:hidden flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <header className="sticky top-0 z-30 flex h-14 items-center px-4 border-b bg-background gap-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
           <Sheet>
